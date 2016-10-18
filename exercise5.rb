491b58be595bd5ce2130a9e0ceeaaf4ef750e155
LUKE PLOURDE <lukeplourde@hotmail.com>
2016-10-18 17:48:34 -0400

    Fixed small typo

--- exercise5.rb
+++ exercise5.rb
@@ -1,9 +1,9 @@
 
-puts "Please enter a teperature in  Fahrenheit"
+puts "Please enter a temperature in  Fahrenheit"
 f = gets.chomp.to_i
 
 def convert_to_c(fahrenheit)
-  return ((fahrenheit - 32) * 5 / 9) 
+  return ((fahrenheit - 32) * 5 / 9)
 end
 
 puts "Your answer is #{convert_to_c(f)}"
