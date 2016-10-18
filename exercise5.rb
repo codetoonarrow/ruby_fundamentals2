0c6d1367807b26285ec8554a3743a3821ea5e5aa
LUKE PLOURDE <lukeplourde@hotmail.com>
2016-10-18 17:37:11 -0400

    Fahrenheit converted to Celsius

---
+++ exercise5.rb
@@ -0,0 +1,9 @@
+
+puts "Please enter a teperature in  Fahrenheit"
+f = gets.chomp.to_i
+
+def convert_to_c(fahrenheit)
+  return ((fahrenheit - 32) * 5 / 9) 
+end
+
+puts "Your answer is #{convert_to_c(f)}"
