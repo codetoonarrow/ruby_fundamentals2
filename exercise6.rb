7d6e84818e626021968f92203acbeb082217ac1f
LUKE PLOURDE <lukeplourde@hotmail.com>
2016-10-19 14:25:00 -0400

    stored alphabetically

--- exercise6.rb
+++ exercise6.rb
@@ -1,12 +1,19 @@
 grocery_list = ["carrots", "toliet paper", "apples", "salmon"]
 grocery_list << "rice"
 
-
-grocery_list.each do |asterisk|
-  puts "* " + asterisk
+#Adding the asterisk to list.
+def shop(list)
+  list.each do |item|
+  puts "* " + "#{item}"
+  end
 end
+shop(grocery_list)
 
+#Counted the number item's on the list.
 puts grocery_list.count
+
+
+#Checked the list if it has banana's.
 
 if grocery_list.include? ("banana")
   puts "You don't need to pick up bananas today"
@@ -14,4 +21,10 @@ else
   puts "You need to pick up bananas"
 end
 
-puts grocery_list[1]
+#Pulled the second item from the list.
+puts "* " + grocery_list[1]
+puts "--------------------"
+
+# Stored list alphabetically.
+
+shop(grocery_list.sort)
