4d07a9fad14bdc501e3b2f77d8bef829f1f96507
LUKE PLOURDE <lukeplourde@hotmail.com>
2016-10-19 15:42:46 -0400

    Increased class size by 5%

--- exercise7.rb
+++ exercise7.rb
@@ -4,10 +4,31 @@ students = {
   :cohort3 => 22
 }
 
+puts "-----------------------"
+teachers = {
+  :cohort1 => 1,
+  :cohort2 => 3,
+  :cohort3 => 5
+}
+
 students.each do |k, v|
   puts "#{k}: #{v}"
 end
 
+puts "-----------------------"
 
 students.merge!(cohort4: 43)
-puts students
+ puts students.keys
+
+puts "-----------------------"
+
+def expand(s)
+  s.each do |k, v|
+    puts (v * 0.05 + v).to_i
+
+  end
+end
+
+expand(students)
+
+puts "-----------------------"
